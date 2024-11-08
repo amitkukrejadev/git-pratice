cfb154a6dde5bda62adf6e753d979cea81d32159
Amit Kukreja <thenameisamitkukreja@gmail.com>
2024-11-08 21:13:22 +0530

    Added Contact Py File

---
+++ contact.py
@@ -0,0 +1,15 @@
+# Contact Me - Content
+
+print("Hello there!")
+print("You can contact me through the following methods:")
+
+linkedin = "https://www.linkedin.com/in/amitkukrejadev/"
+github = "https://github.com/amitkukrejadev"
+email = "amitkukrejadev@gmail.com"
+print(f"Connect With Me On My Linkedin: {linkedin}")
+print(f"Check Out My Projects On My Github: {github}")
+print(f"Send Me An Email: {email}")
+
+
+print("\nFeel free to reach out for any queries or opportunities!")
+print("Looking forward to connecting with you!")
